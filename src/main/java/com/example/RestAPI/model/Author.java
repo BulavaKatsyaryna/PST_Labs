e08dd13b7cf0aa09767587ac9d548d01c574d568
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-05 16:10:46 +0300

    Added sorting by Surname of the author

--- src/main/java/com/example/RestAPI/model/Author.java
+++ src/main/java/com/example/RestAPI/model/Author.java
@@ -9,11 +9,19 @@ import javax.persistence.Column;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Author {
+public class Author implements Comparable<Author>{
 
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
     private String lastName;
+
+    @Override
+    public int compareTo(Author o) {
+        if (getLastName() == null || o.getLastName() == null) {
+            return 0;
+        }
+        return getLastName().compareTo(o.lastName);
+    }
 }
